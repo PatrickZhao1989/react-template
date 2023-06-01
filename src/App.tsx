d91ecc5211265906d896015b7e5cd62ce4c2bf15
Patrick Zhao <patrick.zhao1989@outlook.com>
2023-06-01 17:18:55 +1000

    learn react

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+interface MyButtonInput {
+	count: any;
+	onClick: any;
+}
+
 function App() {
+	const [count, setCount] = useState(0);
+
+	function handleClick() {
+		setCount(count + 1);
+	}
+
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit
-					<code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+			<MyButton count={count} onClick={handleClick} />
+			<MyButton count={count} onClick={handleClick} />
 		</div>
 	);
+}
+
+function MyButton(myButtonInput: MyButtonInput) {
+	return <button onClick={myButtonInput.onClick}>Clicked {myButtonInput.count} times</button>;
 }
 
 export default App;
